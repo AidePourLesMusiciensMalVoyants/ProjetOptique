a7a174bc4fb4dbfc00ea3b6ad1d89d69c61bd821
Christian Arnault <arnault@lal.in2p3.fr>
2019-04-17 16:46:33 +0200

    Test programm to compute speeds Add image to the GitHub organization

---
+++ application/speed.py
@@ -0,0 +1,67 @@
+
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import random
+
+
+class Speed:
+    def __init__(self, f, step):
+        self.dt = 0
+        self.prev = None
+        self.value = None
+        self.step = step
+        self.old = None
+        self.func = f
+
+    def f(self, t):
+
+        # x = math.sin(t)
+
+        if self.dt == 0:
+            if self.prev is None:
+                self.prev = func()
+                self.value = func()
+                self.old = self.value
+
+        vx = 0
+        x = None
+        if self.dt < self.step:
+            vx = (self.value - self.prev) / self.step
+            x = self.prev + (vx * self.dt)
+            self.dt += 1
+            # print(self.prev, self.dt, x, self.value)
+        else:
+            vx = (self.value - self.prev) / self.step
+            self.prev = self.value
+            x = self.value
+            self.dt = 0
+            # print(self.prev, x, self.value)
+            self.value = func()
+
+        self.old = self.value
+
+        return x, vx*20
+
+x = []
+vx = []
+
+
+def func():
+    # return random.random()
+    return math.sin(t)
+
+a = Speed(func, 100.)
+
+for t in range(5000):
+    value, speed = a.f(t/90.)
+    x.append(value)
+    vx.append(speed)
+
+
+fig, ax = plt.subplots()
+ax.plot(x)
+ax.plot(vx)
+plt.show()
+
